ea5b5ed2f0266de2fc8628d0dbcd8d65203cca0e
jaya27200 <jaya22@navgurukul.org>
2024-06-25 21:04:38 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import React from 'react';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
-import './App.css';
-import Form from './Component/Form';
-import Navbar from './Component/Navbar';
-import Landind from './Component/Landind';
-import AddImg from './Component/AddImg';
-import React from 'react';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";  // Ensure CSS import is correct
 import Form from './Component/Form';
 import Navbar from './Component/Navbar';
 import Landind from './Component/Landind';
@@ -24,17 +17,8 @@ function App() {
           <Route path="/editor/*" element={<AddImg />} />
         </Routes>
       </BrowserRouter >
-      <BrowserRouter >
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landind />} />
-          <Route path="/login-signup" element={<Form />} />
-          <Route path="/editor/*" element={<AddImg />} />
-        </Routes>
-      </BrowserRouter >
     </>
   );
-  
-}
 
+}
 export default App;
