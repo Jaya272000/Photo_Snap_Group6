28a49dba20463acf0fde6026c8266b6f4d0233ad
jaya27200 <jaya22@navgurukul.org>
2024-06-25 20:35:28 +0530

    Commit changes

--- src/Component/AddImg.jsx
+++ src/Component/AddImg.jsx
@@ -15,19 +15,3 @@ function AddImg() {
 
 export default AddImg;
 
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import ImageGallery from './ImageGallery';
-import DisplayImage from './DisplayImage';
-
-function AddImg() {
-  const [photo, setPhoto] = useState(null);
-  return (
-    <Routes>
-      <Route path="/" element={<ImageGallery setPhoto={setPhoto} />} />
-      <Route path="/display" element={<DisplayImage photo={photo} />} />
-    </Routes>
-  );
-}
-
-export default AddImg;
